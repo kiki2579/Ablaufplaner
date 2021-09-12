abb73c21f14137816fcbbf17e26c33120f70a083
Felix Tylla <fefe3002@outlook.de>
2021-09-13 00:13:30 +0200

    Zeitanpassung v1.0
    Fehler zwischen am und pm. Rechenfelher

--- JesusHouseAblaufplaner/Form1.cs
+++ JesusHouseAblaufplaner/Form1.cs
@@ -518,7 +518,7 @@ namespace JesusHouseAblaufplaner
             Int32.TryParse(string.Concat(content[1][0][3], content[1][0][4]), out tableminutes);// prespoin +1 weil wir uns auf die verbleibende zeit beziehen. also auf die startzeit des nächsten
 
             date = DateTime.Now;
-            Console.WriteLine($"Form1_Test_Clock: {tablehours}");
+            Console.WriteLine($"Form1_Test_Clock: {tableminutes}");
             Console.WriteLine($"Form1_Test2_Clock: {date.Day.ToString()}");
             if (tablehours == 0) // wenn 0:00 dann Nächter Tag
             {
@@ -536,7 +536,48 @@ namespace JesusHouseAblaufplaner
             Console.WriteLine($"TestZeit: {timeBefore}");
             Console.WriteLine($"TestZeit2: {timeSpan.TotalMilliseconds}");
             Console.WriteLine(timeSpan.ToString(@"hh\:mm\:ss"));
-            if (timeBefore > timeSpan.TotalMilliseconds) Console.WriteLine("Zeitanpassung!");
+            if (timeBefore > timeSpan.TotalMilliseconds)
+            {
+                DialogResult result;
+                result = MessageBox.Show(language.Deutsch.delay.msg, language.Deutsch.delay.caption, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Cancel)
+                {
+                    return;
+                }else if (result == DialogResult.Yes){
+
+                    date = DateTime.Now;
+
+                    for (int i = 1; i <= content.Length; i++)
+                    {
+                        try
+                        {
+                            Int32.TryParse(string.Concat(content[i][0][0], content[i][0][1]), out tablehours);//string1 + string2 - > int
+                            Int32.TryParse(string.Concat(content[i][0][3], content[i][0][4]), out tableminutes);// prespoin +1 weil wir uns auf die verbleibende zeit beziehen. also auf die startzeit des nächsten
+                                                        
+                            Console.WriteLine($"Form12_Test_Clock: {tablehours}");
+                            Console.WriteLine($"Form12_Test2_Clock: {date.Hour.ToString()}");
+                            /*if (tablehours == 0) // wenn 0:00 dann Nächter Tag
+                            {
+                                date = date.AddDays(1);
+                            }*/
+                            Console.WriteLine($"Form12_Test3_Clock: {date.Day.ToString()}");
+                            startdate = new DateTime(Convert.ToInt32(date.ToString("yyyy")), Convert.ToInt32(date.ToString("MM")), Convert.ToInt32(date.ToString("dd")), tablehours, tableminutes, 0);                       
+                            timeSpan = startdate - date;
+                            Console.WriteLine(startdate.ToString(@"hh\:mm\:ss"));
+                            startdate.Add(timeSpan);
+                            Console.WriteLine(timeSpan.ToString(@"hh\:mm\:ss"));
+                            Console.WriteLine(startdate.ToString(@"hh\:mm\:ss"));
+                            content[i][0] = startdate.ToString(@"hh\:mm");
+                            Console.WriteLine(content[i][0]);
+                        }
+                        catch
+                        {
+
+                        }
+
+                    }
+                }
+            }
 
             Console.WriteLine(table_lines);
             Form2 Form2 = new Form2(table_lines, content, table.RowCount);
