a0a735c38956d402ab42ec4ad99ba0fa27f4b453
Timo L <timo.lukowski@icloud.com>
2021-09-27 17:28:57 +0200

    ui form 2 v.1.00

--- JesusHouseAblaufplaner/Form2.cs
+++ JesusHouseAblaufplaner/Form2.cs
@@ -43,15 +43,15 @@ namespace JesusHouseAblaufplaner
             if (timeBefore < timeSpan.TotalSeconds) hoch = true;
             else hoch = false;
             if (!hoch)
-                label5.Text = timeSpan.ToString(@"hh\:mm\:ss");
-            else label5.Text = "+" + timeSpan.ToString(@"hh\:mm\:ss");
+                label8.Text = timeSpan.ToString(@"hh\:mm\:ss");
+            else label8.Text = "+" + timeSpan.ToString(@"hh\:mm\:ss");
             //Console.WriteLine(timeSpan.ToString(@"hh\:mm\:ss"));
             if (!hoch && timeSpan.TotalSeconds > 60)
-                label5.ForeColor = Color.Green;
+                label8.ForeColor = Color.Green;
             else if (!hoch && timeSpan.TotalSeconds < 60)
-                label5.ForeColor = Color.DarkOrange;
+                label8.ForeColor = Color.DarkOrange;
             if (hoch || timeSpan.TotalSeconds <= overtime_indikator)
-                label5.ForeColor = Color.Red;
+                label8.ForeColor = Color.Red;
             if (timeSpan.TotalSeconds < 1) hoch = true;
             timeBefore = timeSpan.TotalSeconds;
         }
@@ -103,7 +103,7 @@ namespace JesusHouseAblaufplaner
             //    style.Height = 50;
             //}
 
-            TableLayoutRowStyleCollection styles = tableLayoutPanel1.RowStyles;
+            TableLayoutRowStyleCollection styles = tableLayoutPanel3.RowStyles;
             foreach (RowStyle style in styles)
             {
                 // Set the row height to 20 pixels.
@@ -125,7 +125,7 @@ namespace JesusHouseAblaufplaner
                     label.Text = $"Was: {table[prespoint + 1 + i][1]}\nWer: {table[prespoint + 1 + i][2]}\nUm: {table[prespoint + 1 + i][0]}\n({table[prespoint + 1 + i][3]})\n";
                     label.Font = new Font("Arial", 11);
                     Console.WriteLine(label.Text);
-                    TableLayoutControlCollection controls = tableLayoutPanel1.Controls;
+                    TableLayoutControlCollection controls = tableLayoutPanel3.Controls;
                     controls.Add(label);
                     Console.WriteLine(table[i][2] + " : " + lines);
                     /*Label label1 = new Label();
@@ -153,18 +153,20 @@ namespace JesusHouseAblaufplaner
             Console.WriteLine(timeSpan.ToString(@"hh\:mm\:ss"));
             timer1.Enabled = true;
             label4.Text = table[prespoint][2];
-            tableLayoutPanel1.Enabled = true;
-            panel1.Enabled = true; 
+            label5.Text = table[prespoint][3];
+            tableLayoutPanel3.Enabled = true;
+            //panel1.Enabled = true; 
             
         }
 
         public void Reload()
-        {
+        {   
+
             timer1.Enabled = false;
-            for (int i = 0; i < tableLayoutPanel1.RowCount; i++)
+            for (int i = 0; i < tableLayoutPanel3.RowCount; i++)
             {
-                tableLayoutPanel1.Controls.RemoveByKey("next" + Convert.ToString(i));
-                tableLayoutPanel1.Controls.RemoveByKey("placeholder" + Convert.ToString(i));
+                tableLayoutPanel3.Controls.RemoveByKey("next" + Convert.ToString(i));
+                tableLayoutPanel3.Controls.RemoveByKey("placeholder" + Convert.ToString(i));
             }
 
             for (int i = 1; i < lines; i++)
@@ -180,7 +182,7 @@ namespace JesusHouseAblaufplaner
                 if (table[prespoint + i][1] != null) label.Text = $"Was: {table[prespoint + i][1]}\nWer: {table[prespoint + i][2]}\nUm: {table[prespoint + i][0]}\n({table[prespoint + i][3]})\n{label.Name}";
                 label.Font = new Font("Arial", 11);
                 Console.WriteLine(label.Text);
-                TableLayoutControlCollection controls = tableLayoutPanel1.Controls;
+                TableLayoutControlCollection controls = tableLayoutPanel3.Controls;
                 controls.Add(label);
                 Console.WriteLine(table[prespoint + i][2] + " : " + lines);
                 /*Label label1 = new Label();
@@ -198,14 +200,14 @@ namespace JesusHouseAblaufplaner
             catch (NullReferenceException e)
             {
                 //Wenn nichts wéiteres geplant ist!;
-                label5.Text = language.Deutsch.Form2.endOfTable;
-                label5.Font = new Font("Microsoft Sans Serif", this.Size.Width / 30, FontStyle.Bold);
+                label8.Text = language.Deutsch.Form2.endOfTable;
+                label8.Font = new Font("Microsoft Sans Serif", this.Size.Width / 30, FontStyle.Bold);
                 label4.Text = table[prespoint][2];
-                label5.ForeColor = Color.White;
+                label8.ForeColor = Color.White;
                 timer1.Enabled = false;
                 return;
             }
-            label5.Font = new Font("Microsoft Sans Serif", this.Size.Width / 30 + 10, FontStyle.Bold);
+            label5.Text = table[prespoint][3];
             date = DateTime.Now;
             if (tablehours == 0)
             {
@@ -219,6 +221,7 @@ namespace JesusHouseAblaufplaner
             overtime_indikator = timeSpan.TotalSeconds * 0.1;
             timer1.Enabled = true;
             label4.Text = table[prespoint][2];
+            label5.Text = table[prespoint][3];
         }
         
 
@@ -233,11 +236,13 @@ namespace JesusHouseAblaufplaner
                 this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
                 formState.Maximize(this);
                 panel4.BackColor = Color.FromArgb(5, Color.White);
-                label6.BackColor = Color.FromArgb(5, Color.White);
+                label7.BackColor = Color.FromArgb(5, Color.White);
                 panel4.Location = new Point(this.Size.Width / 2 - panel4.Size.Width/2, 57);
                 panel4.Visible = true;
                 timer2.Enabled = true;
                 fullscreen = true;
+                tableLayoutPanel1.Height = this.Size.Height;
+                tableLayoutPanel1.Width = this.Size.Width;
             }
             else if (e.KeyCode == Keys.Escape)
             {
@@ -281,7 +286,6 @@ namespace JesusHouseAblaufplaner
             timer2.Enabled = false;
         }
 
-       
 
         private void Form2_SizeChanged(object sender, EventArgs e)
         {
@@ -290,12 +294,18 @@ namespace JesusHouseAblaufplaner
                 //label1.TextAlign = ContentAlignment.BottomRight;
                 this.FormBorderStyle = FormBorderStyle.Sizable;
                 //label1.TextAlign = ContentAlignment.TopCenter;
+                tableLayoutPanel1.Height = 595;
+                tableLayoutPanel1.Width = 365;
             }
+            tableLayoutPanel1.Height = this.Size.Height-30;
+            tableLayoutPanel1.Width = this.Size.Width-10;
             label1.Font = new Font("Microsoft Sans Serif", this.Size.Width / 40, FontStyle.Underline);
             label2.Font = new Font("Microsoft Sans Serif", this.Size.Width / 30, FontStyle.Underline);
             label3.Font = new Font("Microsoft Sans Serif", this.Size.Width / 30);
             label4.Font = new Font("Microsoft Sans Serif", this.Size.Width / 30, FontStyle.Bold);
-            label5.Font = new Font("Microsoft Sans Serif", this.Size.Width / 30 + 10, FontStyle.Bold);
+            label5.Font = new Font("Microsoft Sans Serif", this.Size.Width / 30);
+            label7.Font = new Font("Microsoft Sans Serif", this.Size.Width / 35, FontStyle.Underline);
+            label8.Font = new Font("Microsoft Sans Serif", this.Size.Width / 15);
 
             /*for (int i = 0; i <= lines; i++)
             {
