5d34d7f41cd26e3792306bc4f60781ff8cf03b7f
Timo L <timo.lukowski@icloud.com>
2021-09-27 23:41:09 +0200

    hotfix ui form 2 v1.00

--- JesusHouseAblaufplaner/Form2.Designer.cs
+++ JesusHouseAblaufplaner/Form2.Designer.cs
@@ -244,8 +244,7 @@
             this.tableLayoutPanel3.ForeColor = System.Drawing.SystemColors.Control;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(449, 41);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
-            this.tableLayoutPanel3.RowCount = 400;
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.RowCount = 399;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
